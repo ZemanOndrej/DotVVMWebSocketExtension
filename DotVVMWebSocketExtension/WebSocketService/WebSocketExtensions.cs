93892d23709a7a36662dc729fc9172c00666a073
Ondrej Zeman <ozeman77@gmail.com>
2017-12-04 16:41:42 +0100

    new: documentation for websocket extension;

--- DotVVMWebSocketExtension/WebSocketService/WebSocketExtensions.cs
+++ DotVVMWebSocketExtension/WebSocketService/WebSocketExtensions.cs
@@ -6,15 +6,25 @@ namespace DotVVMWebSocketExtension.WebSocketService
 {
 	public static class WebSocketExtensions
 	{
+
+		/// <summary>
+		/// Adds services needed for WebSocket Extension for DotVVM
+		/// </summary>
+		/// <param name="services">The services.</param>
+		/// <returns></returns>
 		public static IServiceCollection AddWebSocketManagerService(this IServiceCollection services)
 		{
 			services.TryAddSingleton<WebSocketManagerService>();
 			services.TryAddSingleton<WebSocketViewModelSerializer>();
 			services.TryAddScoped<WebSocketHub>();
-
 			return services;
 		}
 
+		/// <summary>
+		/// Maps url starting with "/ws" for the WebSocket middleware.
+		/// </summary>
+		/// <param name="app">The application.</param>
+		/// <returns></returns>
 		public static IApplicationBuilder MapWebSocketService(this IApplicationBuilder app)
 		{
 			return app.Map("/ws", a => a.UseMiddleware<WebSocketMiddleware>());
